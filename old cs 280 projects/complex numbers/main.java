4c6b0381079f005923ef911dba252aa7f7049689
virtualprodigy <virtualprodigyllc@Matthews-MacBook-Pro.local>
2014-07-07 20:26:12 -0400

    added old college projects
    continuing to update samples and my portfolio. I added some projects
    that are listed on my resume. I will replace the projects with a link
    on the resume

---
+++ old cs 280 projects/complex numbers/main.java
@@ -0,0 +1,15 @@
+//Matthew Butler
+//CS 280: Programming Languages Concepts
+// Section CS - 280-102
+// Professor Kapleau
+public class main {
+	public static void main(String [] args){
+		complexNumbers comNumsOne = new complexNumbers();
+		complexNumbers comNumstwo = new complexNumbers();
+		complexNumbers comNumsThree = new complexNumbers();
+		comNumsOne.complexNumber(4,5,7,8);
+		comNumsOne.complexNumber(10,20,3,16);
+		comNumsOne.complexNumber(7,5,7,-56);
+		comNumsOne.complexNumber(-21,-3,5,-24);
+	}
+}
